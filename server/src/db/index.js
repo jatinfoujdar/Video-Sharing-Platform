7515b4504f1303e350d79e130f1da6a1060bb91d
Jatin Foujdar <jatin321001@gmail.com>
2023-11-18 21:59:15 +0530

    db setup commit

---
+++ server/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const connectDB = async ()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log("DB Connection Failled")
+        console.log(`Error:${error.message}`)
+        process.exit(1)
+
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,5 @@
 import mongoose from "mongoose";
 
 
-( async()=>{
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URL}`)
-        console.log(`Database connected succesfully`);
-    } catch (error) {
-        console.log("Error while with the database",error);
-        console.log("error",error)
-        throw err
-    }
-})()
+
+
