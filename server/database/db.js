00f5f14c147f045bea45f091e85f0b39b8b3b6a3
Jatin Foujdar <jatin321001@gmail.com>
2023-11-19 21:42:20 +0530

    cors commit

--- server/database/db.js
+++ server/database/db.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log("DB Connection Failled")
-        console.log(`Error:${error.message}`)
+        console.error("Failed to connect to MongoDB:", error.message);
+
         process.exit(1)
 
     }
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import { connectDB } from "./database/db.js";
 
 dotenv.config();
 connectDB();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 
