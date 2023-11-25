3179a0ceec75bc1945f85db1c523ff485aebb69c
Jatin Foujdar <jatin321001@gmail.com>
2023-11-25 21:19:50 +0530

    token commit

--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -54,4 +54,37 @@ userSchema.pre("save", async function (next) {
     this.password = bcrypt.hash(this.password, 10)
     next();
 })
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.method.generateAccessToken = function(){
+
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.method.generateRefereshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFERESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFERESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
