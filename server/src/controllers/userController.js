5118b3ac6555511ddc43c71d596823aa1da9d2f4
Jatin Foujdar <jatin321001@gmail.com>
2023-12-24 23:01:58 +0530

    empty string commit

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,17 +1,22 @@
+import { apiError } from "../utils/apiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 
 
 export const registerUser = asyncHandler(async (req, res) => {
-    try {
-        
-        res.status(200).json({
-            message: "User registration successful",
-        });
-    } catch (err) {
-        console.error("Error in registerUser:", err);
-       
-        throw err;
+    
+    const {fullName,email,username,password} = req.body;
+    // console.log("email",email);
+
+    // if(fullName === ""){
+    //     throw new apiError(400, " Full name is required")
+    // }
+    if(
+        [fullName,email,username,password].some((field)=>
+         field?.trim() === "")
+    ){
+        throw new apiError(400,"All Fields are required")
     }
+
 });
 
