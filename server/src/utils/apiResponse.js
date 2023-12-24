1cda07d3c81783b4c06be65cbe16cd9c5ea7b012
Jatin Foujdar <jatin321001@gmail.com>
2023-12-24 19:25:18 +0530

    dev commit

--- server/src/utils/apiResponse.js
+++ server/src/utils/apiResponse.js
@@ -1,10 +1,12 @@
 
-export class apiResponse{
+class apiResponse{
     constructor(statusCode,data,message = "Success"){
         this.statusCode= statusCode
         this.data = data
         this.message = message
-        this.sucess = statusCode
+        this.success = statusCode
 
     }
 }
+
+export {apiResponse};
